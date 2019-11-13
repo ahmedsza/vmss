5ba74eb6c075ceb06aec1b10a0de1dc995ae78c9
ahmedsza <37135205+ahmedsza@users.noreply.github.com>
2019-11-13 12:01:36 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -23,4 +23,4 @@
 # SOFTWARE.
 
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from anither custom change 2  $($env:computername) !"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from anither custom change 3 ..  $($env:computername) !"
